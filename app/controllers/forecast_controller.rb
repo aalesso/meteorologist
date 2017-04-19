50d0e7b12e51cef6406f196487a61797691ab6c2
aalesso <aalesso@users.noreply.github.com>
2017-04-19 15:38:39 -0500

    latest

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -15,18 +15,16 @@ class ForecastController < ApplicationController
     # The latitude the user input is in the string @lat.
     # The longitude the user input is in the string @lng.
     # ==========================================================================
+    url1="https://api.darksky.net/forecast/fa5c8431499b20450e20b94e60bd5aa6/"+@lat+","+@lng
+    parsed_data1 = JSON.parse(open(url1).read)
+    # latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    # longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
-
-
-    @current_temperature = "Replace this string with your answer."
-
-    @current_summary = "Replace this string with your answer."
-
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
-
-    @summary_of_next_several_hours = "Replace this string with your answer."
-
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @current_temperature = parsed_data1["currently"]["temperature"]
+    @current_summary = parsed_data1["currently"]["summary"]
+    @summary_of_next_sixty_minutes = parsed_data1["minutely"]["summary"]
+    @summary_of_next_several_hours = parsed_data1["hourly"]["summary"]
+    @summary_of_next_several_days = parsed_data1["daily"]["summary"]
 
     render("forecast/coords_to_weather.html.erb")
   end
